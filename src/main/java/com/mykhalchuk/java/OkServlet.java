49cb7e79a9201233e447c8ed09777b09af3ccbeb
Alexander Mikhal'chuk <am@anahoret.com>
2015-01-17 13:56:44 +0200

    Added Ok servlet.

---
+++ src/main/java/com/mykhalchuk/java/OkServlet.java
@@ -0,0 +1,16 @@
+package com.mykhalchuk.java;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/ok")
+public class OkServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Do nothing
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<body>
+  <h3>Test Links</h3>
+  <ul>
+  <li><a href="ok">ok 200</a></li>
+  </ul>
+</body>
+</html>
